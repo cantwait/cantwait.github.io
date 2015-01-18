b42a68d20e08e6acb2e261f20409957f658ee425
cantwait <cadenas.rafael@gmail.com>
2015-01-18 16:06:57 -0500

    changes to view

--- app/scripts/directives/yoda-directive.js
+++ app/scripts/directives/yoda-directive.js
@@ -7,7 +7,7 @@ angular.module('admiosTestProjectApp')
 		name: 'my-yoda',
 		// priority: 1,
 		// terminal: true,
-		scope: true, // {} = isolate, true = child, false/undefined = no change
+		//scope: true, // {} = isolate, true = child, false/undefined = no change
 		// controller: function($scope, $element, $attrs, $transclude) {},
 		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
 		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
