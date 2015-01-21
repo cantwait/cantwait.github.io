e9248f879a24ae9762700275faef7187d358ceda
cantwait <cadenas.rafael@gmail.com>
2015-01-21 17:49:29 -0500

    e2e added

--- test/e2e/index/index.specs.js
+++ test/e2e/index/index.specs.js
@@ -1,7 +1,25 @@
 describe("yoda-app-protactor-tests", function() {
+	beforeEach(function(){
+		browser.get('http://localhost:9000/#/');
+	});
+
 	it('should show a title', function(){
-	browser.get('http://localhost:9000/#/');
-	expect(browser.getTitle()).toEqual('Sample Application');
+		expect(browser.getTitle()).toEqual('Sample Application');
+	});
+
+	it('should have a h3 with text', function(){
+		var yodaBtn = element(by.id('yodaBtn'));
+		var inputText = element(by.id('inputText'));
+		var pElement = element(by.id('success'));
+		expect(yodaBtn.getText()).toBe('try me');
+
+		//send text
+		inputText.sendKeys('I will help you');
+
+		yodaBtn.click();
+
+		expect(pElement.getText()).not.toBeNull();
+
 	});
 });
 	
